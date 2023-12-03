7ae61d6e8e6bf7dd8c621df26c5c64c2024fd480
19levi19 <amanchaudhur29902@gmail.com>
2023-12-03 18:21:35 +0530

    created models actions for threads

---
+++ lib/actions/threads.actions.ts
@@ -0,0 +1,102 @@
+"use server"
+
+import { connectToDB } from "../mongoose";
+import Thread from "../models/thread.model";
+import User from "../models/user.model";
+import { revalidatePath } from "next/cache";
+interface params {
+   text:string,
+   author:string,
+   communityId:string | null,
+   path:string,
+ 
+}
+
+export async function createThread({text,author,communityId,path}:params) {
+    try {
+        connectToDB();
+
+    const createdThread = await Thread.create({
+        text,
+        author,
+        community:null,
+
+    })
+
+    await User.findByIdAndUpdate(author,
+        {$push:{threads:createdThread._id}
+    })
+
+    revalidatePath(path);
+    } catch (error:any) {
+        throw new Error (`Error creating thread : ${error.message}`)
+    }
+    
+    
+    
+}
+
+export async function fetchPosts(pageNumber = 1,pagesize = 20){
+    connectToDB();
+
+    const skipAmount = (pageNumber -1)* pagesize;
+    const postQuery = Thread.find({parentId: {$in:[null,undefined]}})
+    .sort({createdAt:'desc'})
+    .skip(skipAmount)
+    .limit(pagesize)
+    .populate({path: 'author',model: User})
+    .populate({path:'children',
+    populate:{
+        path:'author',
+        model:User,
+        select :'_id name parentId image'
+
+    }
+
+    })
+
+    const totalPostCount = await Thread.countDocuments({
+        parentId: {$in:[null,undefined]}  
+    })
+
+    const posts = await postQuery.exec();
+
+    const isNext = totalPostCount >skipAmount + posts.length;
+
+    return{posts,isNext};
+}
+
+export async function fetchThreadById (id:string) {
+    connectToDB();
+
+    try {
+        const thread = await Thread.findById(id)
+        .populate({
+            path:'author',
+            model:User,
+            select:"_id id name image"
+        })
+        .populate({
+            path:'children',
+            populate:[
+                {
+                    path:'author',
+                    model:User,
+                    select:"_id id name parentId image"
+                },
+                {
+                    path:'children',
+                    model:Thread,
+                    populate:{
+                        path:'author',
+                        model:User,
+                        select:"_id id name parentId image"
+                    }
+                }
+            ]
+        }).exec();
+        return thread;
+    } catch (error:any) {
+        throw new Error (`Error fetching thread: ${error.msg}`)
+    }
+}
