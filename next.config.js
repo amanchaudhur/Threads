8eb3bc61b6143ee31b7a24153e9acbf848e4f915
19levi19 <amanchaudhur29902@gmail.com>
2023-12-04 16:18:08 +0530

    done with account profile

--- next.config.js
+++ next.config.js
@@ -1,32 +1,20 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverActions: true,
-      serverComponentsExternalPackages: ["mongoose"],
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: [
+      "img.clerk.com",
+      "images.clerk.dev",
+      "uploadthing.com",
+      "placehold.co",
+      "utfs.io", // Add utfs.io here
+    ],
+    typescript: {
+      ignoreBuildErrors: true,
     },
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "img.clerk.com",
-        },
-        {
-          protocol: "https",
-          hostname: "images.clerk.dev",
-        },
-        {
-          protocol: "https",
-          hostname: "uploadthing.com",
-        },
-        {
-          protocol: "https",
-          hostname: "placehold.co",
-        },
-      ],
-      typescript: {
-        ignoreBuildErrors: true,
-      },
-    },
-  };
-  
-  module.exports = nextConfig;
+  },
+};
+
+module.exports = nextConfig;
